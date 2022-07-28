9d8bae2e199793b8665fe185f0858927162697db
davut <davutsh06@gmail.com>
2022-07-28 14:53:19 +0500

    created base.html, added python code in templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'Главная',
+    }
+    return render(request, 'mainapp/index.html', context=context)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'title': 'Продукты',
+    }
+    return render(request, 'mainapp/products.html', context=context)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    context = {
+        'title': 'Контакты',
+    }
+    return render(request, 'mainapp/contact.html', context=context)
