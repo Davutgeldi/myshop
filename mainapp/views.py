98b95f1e46ee78f9a64a2d421a8471916a278e9e
davut <davutsh06@gmail.com>
2022-07-28 15:29:31 +0500

    added loops in templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
 
+MENU_LINKS = {
+    'index': 'Главная',
+    'products': 'Продукты',
+    'contact': 'Контакты',
+}
 def index(request):
     context = {
         'title': 'Главная',
+        'menu': MENU_LINKS,
     }
     return render(request, 'mainapp/index.html', context=context)
 
@@ -11,6 +17,9 @@ def index(request):
 def products(request):
     context = {
         'title': 'Продукты',
+        'menu': MENU_LINKS,
+        'main_product': ['img/controll.jpg', 'img/controll1.jpg', 'img/controll2.jpg'],
+        'products': ['img/product-11.jpg', 'img/product-21.jpg', 'img/product-31.jpg'] 
     }
     return render(request, 'mainapp/products.html', context=context)
 
@@ -18,5 +27,6 @@ def products(request):
 def contact(request):
     context = {
         'title': 'Контакты',
+        'menu': MENU_LINKS,
     }
     return render(request, 'mainapp/contact.html', context=context)
