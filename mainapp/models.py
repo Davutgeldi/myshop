56194f51246022e3f54e45f54dc9794708471391
davut <davutsh06@gmail.com>
2022-07-30 12:53:56 +0500

    created models for project

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=150, blank=True)
+
+
+class Products(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='product_images', blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(default=0)
