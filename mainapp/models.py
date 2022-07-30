6db4f94e6d2ad63d1392d160b0c6ae52f4a3c951
davut <davutsh06@gmail.com>
2022-07-30 14:12:49 +0500

    added local mediafiles, dump data for DB, configuration in settings.py and urls.py for mediafiles

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,6 +5,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=150, blank=True)
 
+    def __str__(self):
+        return self.name 
+
 
 class Products(models.Model):
     name = models.CharField(max_length=150)
@@ -13,3 +16,8 @@ class Products(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Produc : {self.name} | Category : {self.category}'
+
+
